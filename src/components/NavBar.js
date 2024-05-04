cd85c1a1b764c5f8a26565e5b1eac3eb12f1618d
Georgemugo <georgepmash@gmail.com>
2024-05-04 16:25:03 +0300

    Adds validation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{/* display an <a> tag for each link here */}
+  <a key='1' href= '#home'>Home</a>
+  <a key='2' href= '#about'>About</a>
+  <a key='3' href= '#projects'>Projects</a>
+
+  </nav>;
 }
 
 export default NavBar;
